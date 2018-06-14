f2fb695201c67d7c1b6afa210465134b22366e00
dgrishin <box625@gmail.com>
2018-06-14 15:27:44 -0400

    class Item created

---
+++ m1-capstone/src/main/java/Item.java
@@ -0,0 +1,42 @@
+
+public class Item {
+
+	private String name;
+	private double price;
+	private String type;
+	
+	public Item(String name, double price, String type) {
+		this.name = name;
+		this.price = price;
+		this.type = type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSound() {
+		String sound = null;
+		if (type.equals("candy")) sound = "Munch Munch, Yum!";
+		if (type.equals("chip")) sound = "Crunch Crunch, Yum!";
+		if (type.equals("drink")) sound = "Glug Glug, Yum!";
+		if (type.equals("gum")) sound = "Chew Chew, Yum!";
+		return sound;
+		
+	}
+
+}
