9c7f049f37e981f329f936548d011d175f9bb103
dgrishin <box625@gmail.com>
2018-06-15 18:57:50 -0400

    final 1

--- m1-capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ m1-capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,7 +1,13 @@
 package com.techelevator;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -24,26 +30,34 @@ public class VendingMachineCLI {
 
 	private static double balance = 0;
 	Inventory inventory = new Inventory();
+	File logFile = new File("log.txt");
+	String fileName = "Sales Report" + LocalDateTime.now() + ".txt";
+	File salesReport = new File(fileName);
 	Map<String, Slot> intList;
+	Map<Item, Integer> soldItems = new HashMap<Item, Integer>();
 	List<Item> listOfPurchasedItems = new ArrayList<Item>();
-	
-	
+
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
 		this.in = new Scanner(System.in);
 	}
 
-
 	public void run() throws FileNotFoundException {
 		intList = inventory.setInventory();
+		salesReport = new File(fileName);
+		PrintWriter eraser = new PrintWriter(logFile);
+		eraser.print("");
+		eraser.close();
+		try {
+			logFile.createNewFile();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
-
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 				showInventory();
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-
-
 				purchase();
 			}
 		}
@@ -60,7 +74,6 @@ public class VendingMachineCLI {
 		String purchaseChoice = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
 
 		if (purchaseChoice.equals(PURCHASE_MENU_OPTION_FEED_MONEY)) {
-
 			feedMoney();
 			purchase();
 		} else if (purchaseChoice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
@@ -92,16 +105,17 @@ public class VendingMachineCLI {
 		if (!flag) {
 			System.out.println("You entered an invalid bill");
 			feedMoney();
+		} else {
+			balance += (double) input;
+			printBalance(balance);
+			writeToLogFile("FEED MONEY", (double) input);
 		}
-
-		balance += (double)input;
-		System.out.println("Your balance is : " + balance + "  dollar(-s)" );
 	}
 
 	public void sale() {
 		showInventory();
-		System.out.println("Please enter your selection");
-		String userChoice = in.nextLine();
+		System.out.println("Please enter your selection >>>>>");
+		String userChoice = in.nextLine().toUpperCase();
 		boolean flag = false;
 
 		for (Map.Entry<String, Slot> entry : intList.entrySet()) {
@@ -111,61 +125,133 @@ public class VendingMachineCLI {
 			}
 		}
 		if (!flag) {
-			System.out.println("\nYour choice is not recognized\n");
+			System.out.println("\nYour choice is not recognized. Please try again!\n");
 			sale();
 		}
-		if (intList.get(userChoice).qty > 0) {
-			// reduce inventory
-
-			int s = intList.get(userChoice).qty;
-			intList.put(userChoice, new Slot(intList.get(userChoice).item, s - 1));
-			// adding to list of purchased items
-			listOfPurchasedItems.add(intList.get(userChoice).item);
-
-			// reduce balance
-			balance -= (double)intList.get(userChoice).item.getPrice();
-			System.out.println("You got " + intList.get(userChoice).item.getName());
-			System.out.println("Your balance is : " + balance + "  dollar(-s)" );
-			// print log file
+		if (balance >= intList.get(userChoice).getItem().getPrice()) {
+			if (intList.get(userChoice).getQty() > 0) {
+				// reduce inventory
+				int s = intList.get(userChoice).getQty();
+				intList.put(userChoice, new Slot(intList.get(userChoice).getItem(), s - 1));
+				// adding to list of purchased items
+				listOfPurchasedItems.add(intList.get(userChoice).getItem());
+				// reduce balance
+				balance -= intList.get(userChoice).getItem().getPrice();
+				System.out.println("You got " + intList.get(userChoice).getItem().getName());
+				printBalance(balance);
+				// print log file
+				String outputToFile = intList.get(userChoice).getItem().getName() + "  " + userChoice;
+				writeToLogFile(outputToFile, -1 * intList.get(userChoice).getItem().getPrice());
+				writeToSalesReport(intList.get(userChoice).getItem());
+			} else {
+				System.out.println("This item is Sold Out\n");
+				purchase();
+			}
 		} else {
-			System.out.println("This item is Sold Out");
-			sale();
+			System.out.println("Your don't have enough money to buy this item.");
+			System.out.println("Please add money or make another choice");
+			printBalance(balance);
+			purchase();
 		}
 	}
-
 
 	public void finishTransaction() {
 		for (Item i : listOfPurchasedItems) {
 			System.out.println(i.getSound());
 		}
-
+		System.out.println("\nYour change is:\n");
 		// print log file
-
+		double changeAmount = -balance;
 		double[] coins = { 0.25, 0.10, 0.05 };
 		String[] coinNames = { "Quarter ", "Dime ", "Nickel " };
-		
-			for (int j = 0; j < 3; j++) {
-				while (balance > 0) {
-					if (balance >= coins[j]) {
-						balance -= coins[j];
-						System.out.println(coinNames[j]);
-					} else {
+
+		for (int j = 0; j < 3; j++) {
+			while (balance > 0.0) {
+				//balance = Math.round(balance * 1000)/1000;
+				if (balance >= coins[j]) {
+					balance -= coins[j];
+					System.out.println(coinNames[j]);
+				} else {
 					break;
-					}
-				}		
+				}
 			}
+		}
+		writeToLogFile("GIVE CHANGE", changeAmount);
 	}
 
 	public void showInventory() {
-		System.out.println("Slot     Item Name         Price   " );
-		System.out.println("===================================" );
+		System.out.println("Slot    Item Name           Price   ");
+		System.out.println("===================================");
 		for (Map.Entry<String, Slot> entry : intList.entrySet()) {
 			String key = entry.getKey();
 			Slot value = entry.getValue();
-			String output = String.format("%-5s %-25s %-12s", key, value.item.getName(), value.item.getPrice());
+			String output = String.format("%-5s %-20s $%6.2f", key, value.getItem().getName(), value.getItem().getPrice());
 			System.out.println(output);
 		}
 	}
+
+	public void printBalance(double b) {
+		System.out.println(String.format("%-15s $%6.2f", "Your balance is : ", b));
+	}
+
+	public void writeToLogFile(String actionType, double balanceChange) {
+		try {
+			PrintWriter writer = new PrintWriter(new FileWriter(logFile, true));
+			String output = String.format("%-5s %-10s $%4.2f $%4.2f", LocalDateTime.now(), actionType,
+					balance - balanceChange, balance);
+			writer.println(output);
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void writeToSalesReport(Item item) {
+		if (!soldItems.containsKey(item)) {
+			soldItems.put(item, 0);
+		}
+
+		soldItems.put(item, soldItems.get(item) + 1);
+
+		PrintWriter eraser;
+		try {
+			eraser = new PrintWriter(salesReport);
+			eraser.print("");
+			eraser.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		double totalSales = 0;
+		for (Map.Entry<Item, Integer> entry : soldItems.entrySet()) {
+			Item key = entry.getKey();
+			Integer value = entry.getValue();
+			String output = key.getName() + " | " + value;
+			totalSales += key.getPrice() * value;
+			try {
+				PrintWriter writer = new PrintWriter(new FileWriter(salesReport, true));
+				writer.println(output);
+				writer.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		try {
+			PrintWriter writer = new PrintWriter(new FileWriter(salesReport, true));
+			writer.print("\n**TOTALSALES**  $");
+			writer.println(totalSales);
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 }
-
-
